2966ee996edbf579757c6534eb6715b46ece9c6c
Stefano Sarioli <stefano.sarioli@gmail.com>
2015-04-01 14:56:23 +0200

    Complete chapter 8 without tail recursion exercise, no tail recursion in js

---
+++ 8Records&Iterator&TailRecursion/README.txt
@@ -0,0 +1,2 @@
+There is no tail recursion optimization on javascript,
+so i don't do the exercise about the tail recursion.
---
+++ 8Records&Iterator&TailRecursion/ch8.js
@@ -0,0 +1,27 @@
+function valv(name, grade) {
+  return {stud: name, voto: grade};
+}
+
+function valuta(record) {
+  if (record.voto < 18) {
+    return {id: record.stud, giudizio: 'insufficiente'};
+  }
+  if (record.voto <= 22) {
+    return {id: record.stud, giudizio: 'sufficiente'};
+  }
+  if (record.voto <= 27) {
+    return {id: record.stud, giudizio: 'buono'};
+  }
+  return {id: record.stud, giudizio: 'ottimo'};  
+}
+
+function valutaListr(listOFRecords) {
+  return listOFRecords.map(valuta);
+}
+
+function creaValListR(studenti, voti) {
+  if(studenti[0] === undefined) {
+    return [];
+  }
+  return [{stud: studenti[0], voto: voti[0]}].concat(creaValListR(studenti.slice(1, studenti.length), voti.slice(1, studenti.length)));
+}
