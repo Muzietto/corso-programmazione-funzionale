e9f56d36218d3e7f8397fffb1a0df96490f44072
Stefano Sarioli <stefano.sarioli@gmail.com>
2015-04-01 17:34:46 +0200

    presetted test for chapter 11 done, working fold and foldl

--- 11HighOrder&Fold/ch11.js
+++ 11HighOrder&Fold/ch11.js
@@ -1,4 +1,76 @@
-
-function sumWithFoldl(array){
-  return foldl(array,function(acc,curr){ return acc + curr; },0);
+function sumWithFoldl(array) {
+  return foldl(array, function(acc,curr){ return acc + curr; }, 0);
 }
+
+function mulWithFoldl(array) {
+  return foldl(array, function(acc,curr){ return acc * curr; }, 1);
+}
+
+function countWithFoldl(array) {
+  return foldl(array, function(acc,curr){ return acc + 1; }, 0);
+}
+
+function maxWithFoldl(array) {
+  return foldl(array, function(acc,curr){ 
+        if (acc > curr) {
+          return acc;
+        }
+        return curr;
+      }, -Math.pow(2,32) - 1);
+}
+
+function minWithFoldl(array) {
+  return foldl(array, function(acc,curr){ 
+        if (acc < curr) {
+          return acc;
+        }
+        return curr;
+      }, Math.pow(2,32) - 1);
+}
+
+function revWithFoldl(array) {
+  return foldl(array, function(acc,curr){ 
+        acc.unshift(curr);
+        return acc;
+      }, []);
+}
+
+function sumWithFold(array) {
+  return fold(array, function(acc, curr){ return acc + curr; }, 0);
+}
+
+function revWithFold(array) {
+  return fold(array, function(acc, curr){ 
+        acc.push(curr);
+        return acc;
+      }, []);
+}
+
+function mulWithFold(array) {
+  return fold(array, function(acc, curr){ return acc * curr; }, 1);
+}
+
+function countWithFold(array) {
+  return fold(array, function(acc,curr){ return acc + 1; }, 0);
+}
+
+function maxWithFold(array) {
+  return fold(array, function(acc,curr){ 
+        if (acc > curr) {
+          return acc;
+        }
+        return curr;
+      }, -Math.pow(2,32) - 1);
+}
+
+function minWithFold(array) {
+  return fold(array, function(acc,curr){ 
+        if (acc < curr) {
+          return acc;
+        }
+        return curr;
+      }, Math.pow(2,32) - 1);
+}
+
+
+
--- 11HighOrder&Fold/ch11_test.js
+++ 11HighOrder&Fold/ch11_test.js
@@ -9,26 +9,96 @@ describe('in chapter 11',function(){
       expect([1,2,3,4].reduce(adder,0)).to.be.equal(10);
     });
     it('can multiply all elements of a given array',function(){
+      var multiplyer = function(acc,curr){ return acc * curr; };
+      expect(foldl([1,2,3,4],multiplyer,1)).to.be.equal(24);
+      expect(mulWithFoldl([1,2,3,4])).to.be.equal(24);
+      expect([1,2,3,4].reduce(multiplyer,1)).to.be.equal(24);
     });
     it('can count the elements of a given array',function(){
+      var counter = function(acc,curr){ return acc + 1; };
+      expect(foldl([1,2,3,4],counter,0)).to.be.equal(4);
+      expect(countWithFoldl([1,2,3,4])).to.be.equal(4);
+      expect([1,2,3,4].reduce(counter,0)).to.be.equal(4);
     });
     it('can find the min/max element of a given array',function(){
+      var minEl = function(acc,curr){ 
+        if (acc < curr) {
+          return acc;
+        }
+        return curr;
+      };
+      expect(foldl([1,2,3,4],minEl, Math.pow(2,32) - 1)).to.be.equal(1);
+      expect(minWithFoldl([1,2,3,4])).to.be.equal(1);
+      expect([1,2,3,4].reduce(minEl, Math.pow(2,32) - 1)).to.be.equal(1);
+      
+      var maxEl = function(acc,curr){
+        if (acc > curr) {
+          return acc;
+        }
+        return curr;
+      };
+      expect(foldl([1,2,3,4],maxEl, -Math.pow(2,32) - 1)).to.be.equal(4);
+      expect(maxWithFoldl([1,2,3,4])).to.be.equal(4);
+      expect([1,2,3,4].reduce(maxEl, -Math.pow(2,32) - 1)).to.be.equal(4);
     });
     it('can easily reverse a given array',function(){
+      var reverser = function(acc,curr){ 
+        acc.unshift(curr);
+        return acc;
+      };
+      expect(foldl([1,2,3,4],reverser,[])).to.be.eql([4, 3, 2, 1]);
+      expect(revWithFoldl([1,2,3,4])).to.be.eql([4, 3, 2, 1]);
+      expect([1,2,3,4].reduce(reverser,[])).to.be.eql([4, 3, 2, 1]);
     });
   });
   describe('fold RIGHT over arrays (aka fold)',function(){
-    it.skip('can sum all elements of a given array',function(){
+    it('can sum all elements of a given array',function(){
       var adder = function(head,tail){ return head + tail; };
       expect(fold([1,2,3,4],adder,0)).to.be.equal(10);
       expect(sumWithFold([1,2,3,4])).to.be.equal(10);
       expect([1,2,3,4].reduceRight(adder,0)).to.be.equal(10);
     });
+    it('can easily reverse a given array',function(){
+      var reverser = function(acc, curr){ 
+        acc.push(curr);
+        return acc;
+      };
+      expect(fold([1,2,3,4],reverser,[])).to.be.eql([4, 3, 2, 1]);
+      expect(revWithFold([1,2,3,4])).to.be.eql([4, 3, 2, 1]);
+      expect([1,2,3,4].reduceRight(reverser,[])).to.be.eql([4, 3, 2, 1]);
+    });
     it('can multiply all elements of a given array',function(){
+      var multiplyer = function(acc,curr){ return acc * curr; };
+      expect(fold([1,2,3,4],multiplyer,1)).to.be.equal(24);
+      expect(mulWithFold([1,2,3,4])).to.be.equal(24);
+      expect([1,2,3,4].reduceRight(multiplyer,1)).to.be.eql(24);
     });
     it('can count the elements of a given array',function(){
+      var counter = function(acc,curr){ return acc + 1; };
+      expect(fold([1,2,3,4],counter,0)).to.be.equal(4);
+      expect(countWithFold([1,2,3,4])).to.be.equal(4);
+      expect([1,2,3,4].reduceRight(counter,0)).to.be.eql(4);
     });
     it('can find the min/max element of a given array',function(){
+      var minEl = function(acc,curr){ 
+        if (acc < curr) {
+          return acc;
+        }
+        return curr;
+      };
+      expect(fold([1,2,3,4],minEl, Math.pow(2,32) - 1)).to.be.equal(1);
+      expect(minWithFold([1,2,3,4])).to.be.equal(1);
+      expect([1,2,3,4].reduceRight(minEl, Math.pow(2,32) - 1)).to.be.equal(1);
+      
+      var maxEl = function(acc,curr){
+        if (acc > curr) {
+          return acc;
+        }
+        return curr;
+      };
+      expect(foldl([1,2,3,4],maxEl, -Math.pow(2,32) - 1)).to.be.equal(4);
+      expect(maxWithFold([1,2,3,4])).to.be.equal(4);
+      expect([1,2,3,4].reduceRight(maxEl, -Math.pow(2,32) - 1)).to.be.equal(4);
     });
   });
 });
